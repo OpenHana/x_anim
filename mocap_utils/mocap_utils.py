634f74f5204fa617571c6db5fad253b03a2bef86
XuXing <xux660@hotmail.com>
2024-02-20 21:29:45 +0800

    stop button

--- mocap_utils/mocap_utils.py
+++ mocap_utils/mocap_utils.py
@@ -1,9 +1,9 @@
 import bpy
 from bpy.types import Context, Operator, Panel
 
-class x_anim_OT_synced_mocap(Operator):
-    bl_idname = "x_anim.synced_mocap"
-    bl_label = "Synced Mocap"
+class x_anim_OT_synced_mocap_start(Operator):
+    bl_idname = "x_anim.synced_mocap_start"
+    bl_label = "Synced Mocap Start"
     bl_description = "Activate multiple mocap systems at once, (FaceIt, VMC4B)"
 
     def execute(self, context: Context):
@@ -12,6 +12,20 @@ class x_anim_OT_synced_mocap(Operator):
 
         bpy.ops.vmc4b.connect()
         bpy.ops.vmc4b.startrecording()
+
+        return {'FINISHED'}
+    
+class x_anim_OT_synced_mocap_stop(Operator):
+    bl_idname = "x_anim.synced_mocap_stop"
+    bl_label = "Synced Mocap Stop"
+    bl_description = "Stop multiple mocap systems at once, (FaceIt, VMC4B)"
+
+    def execute(self, context: Context):
+        
+        bpy.ops.faceit.receiver_stop()
+
+        bpy.ops.vmc4b.stoprecording()
+        bpy.ops.vmc4b.disconnect()
 
         return {'FINISHED'}
 
@@ -26,4 +40,5 @@ class x_anim_PT_mocap_utils(Panel):
         props = bpy.context.scene.x_anim
         
         row = layout.row()
-        row.operator(x_anim_OT_synced_mocap.bl_idname, text=x_anim_OT_synced_mocap.bl_label)
+        row.operator(x_anim_OT_synced_mocap_start.bl_idname, text=x_anim_OT_synced_mocap_start.bl_label)
+        row.operator(x_anim_OT_synced_mocap_stop.bl_idname, text=x_anim_OT_synced_mocap_stop.bl_label)
